b7537a9c703b5f97f4fe19e893c27c4395803f56
Richard Liu <41549414+its-rich@users.noreply.github.com>
2021-01-08 18:37:07 +1100

    Changed to functional components & added in initial SCSS for products

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../styles/styles.css';
 
-class Home extends React.Component {
+function Home(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return(
-            <div>
-            home
+    return (
+        <div className="main">
+            <div className="even-columns">
+                <div className="col" style={{backgroundColor: "grey", border: "black"}}>
+                    <img style={{maxWidth: "100%", maxHeight: "100%"}} src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Mona_Lisa-restored.jpg" />
+                    <div>
+                    here is some content
+                    </div>
+                </div>
+                <div className="col" style={{backgroundColor: "grey"}}>
+                    <img style={{width: "100px", height: "100px"}} src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Mona_Lisa-restored.jpg" />
+                </div>
+                <div className="col" style={{backgroundColor: "grey"}}>
+                    <img style={{width: "100px", height: "100px"}} src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Mona_Lisa-restored.jpg" />
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Home;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,32 +3,23 @@ import { Route, Switch } from 'react-router-dom';
 import logo from '../images/logo.png';
 import '../styles/styles.css';
 
-class NavBar extends React.Component {
+function NavBar(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return(
-            <header>
-                <div className="navLogo">
-                    <img src={logo} alt="" width="60px"/>
-                </div>
-                <div className="navBrand">
-                    <h1>Green Foods</h1>
-                    <h1>Straight From The Farm</h1>
-                </div>
-                <div className="navSearch">
-                    <input type="text" className="search"/>
-                    <span class="material-icons" style={{color:"black"}}>search</span>
-                </div>
-            </header>
-        );
-    }
+    return (
+        <header>
+            <div className="navLogo">
+                <img src={logo} alt="" width="60px"/>
+            </div>
+            <div className="navBrand">
+                <h1>Green Foods</h1>
+                <h1>Straight From The Farm</h1>
+            </div>
+            <div className="navSearch">
+                <input type="text" className="search"/>
+                <span className="material-icons" style={{color:"black"}}>search</span>
+            </div>
+        </header>
+    );
 }
 
 export default NavBar;
